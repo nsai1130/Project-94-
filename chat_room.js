56d0651f92b6ba7611a5f03c3db6afecc959b86e
nsai1130 <90731926+nsai1130@users.noreply.github.com>
2023-07-20 15:47:50 -0400

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBDIIe9ED7gYk1PzLS2bz9W6uou3oZsPk4",
+    authDomain: "chat-app-c9ef2.firebaseapp.com",
+    projectId: "chat-app-c9ef2",
+    storageBucket: "chat-app-c9ef2.appspot.com",
+    messagingSenderId: "28441747829",
+    appId: "1:28441747829:web:77dcf929ece6e2dd09b33c"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
